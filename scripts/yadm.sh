ca3a9bf65dbc74e075e51d4877bc783fa479b57b
Danny Roberts <danny@thefallenphoenix.net>
2023-06-30 13:41:05 +0100

    Install yadm directly into /usr/bin

--- scripts/yadm.sh
+++ scripts/yadm.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-mkdir -p /usr/local/bin
-curl -fLo /usr/local/bin/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm && chmod a+x /usr/local/bin/yadm
+curl -fLo /usr/bin/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm && chmod a+x /usr/bin/yadm
