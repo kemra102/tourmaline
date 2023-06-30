2c2f5d1d558597d1891ba234953cddda44d068c7
Danny Roberts <danny@thefallenphoenix.net>
2023-06-30 10:55:59 +0100

    Install starship via official script instead of via COPR.

--- scripts/starship.sh
+++ scripts/starship.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
-# Enable starship COPR repo
-AUTHOR='atim'
-REPONAME='starship'
-RELEASE=$(rpm -E %fedora)
-
-curl -fsSL "https://copr.fedorainfracloud.org/coprs/${AUTHOR}/${REPONAME}/repo/fedora-${RELEASE}/${AUTHOR}-${REPONAME}-fedora-.repo" | sudo tee "/etc/yum.repos.d/${AUTHOR}-${REPONAME}.repo"
-rpm-ostree install starship
+curl -sS https://starship.rs/install.sh | sh -s -- --yes --bin-dir /usr/local/bin
