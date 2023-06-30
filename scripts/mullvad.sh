0b1534977c4fafd3f13c9e6c8d74bbbe41f3a5c7
Danny Roberts <danny@thefallenphoenix.net>
2023-06-30 14:54:39 +0100

    Try figuring out hot to get the Mullvad VPN install to work.

--- scripts/mullvad.sh
+++ scripts/mullvad.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+ln -s /var/opt/Mullvad\ VPN /opt/Mullvad\ VPN
+
 filename='mullvad.rpm'
 curl -L https://mullvad.net/download/app/rpm/latest -o "$filename"
 rpm-ostree install -y "$filename"
