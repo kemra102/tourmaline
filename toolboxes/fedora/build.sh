5df9a4ff8335db6d3aae26858da1b862c9ce995b
Danny Roberts <danny@thefallenphoenix.net>
2023-07-11 22:37:09 +0100

    install vscode in the fedora container

--- toolboxes/fedora/build.sh
+++ toolboxes/fedora/build.sh
@@ -8,6 +8,17 @@ dnf update -y
 echo 'Install additional RPM packages...'
 dnf install -y vim-enhanced zsh
 
+echo 'Installing Visual Studio Code...'
+cat << EOF > /etc/yum.repos.d/vscode.repo
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
+dnf install -y code
+
 echo 'Installing Starship prompt...'
 curl -sS https://starship.rs/install.sh | sh -s -- --yes --bin-dir /usr/bin
 
