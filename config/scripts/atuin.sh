733323ae9263439d91b56d3fc1f2ca70307dc55c
Danny Roberts <danny@thefallenphoenix.net>
2023-10-29 15:10:29 +0000

    feature: My initial changes.

---
+++ config/scripts/atuin.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+bash <(curl --proto '=https' --tlsv1.2 -sSf https://setup.atuin.sh)
---
+++ config/scripts/yadm.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+curl -fLo /usr/bin/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm && chmod a+x /usr/bin/yadm
