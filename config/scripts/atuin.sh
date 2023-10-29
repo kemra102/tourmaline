9e4c7d5d9371fef57f015e2bb5a4d30b1e0823da
Danny Roberts <danny@thefallenphoenix.net>
2023-10-29 16:27:42 +0000

    feature: re-enable atuin builds

--- config/scripts/atuin.sh
+++ config/scripts/atuin.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-bash <(curl --proto '=https' --tlsv1.2 -sSf https://setup.atuin.sh)
+ATUIN_VERSION='17.0.1'
+TMP_DIR=$(mktemp -d)
+
+curl -s "https://github.com/atuinsh/atuin/releases/download/v${ATUIN_VERSION}/atuin-v${ATUIN_VERSION}-x86_64-unknown-linux-gnu.tar.gz" | tar -xzf - -C "$TMP_DIR"
+
+cp "${TMP_DIR}/atuin-v${ATUIN_VERSION}-x86_64-unknown-linux-gnu/atuin" /usr/bin/
+cp "${TMP_DIR}/atuin-v${ATUIN_VERSION}-x86_64-unknown-linux-gnu/completions/atuin.bash" /usr/share/bash-completion/completions/atuin
+cp "${TMP_DIR}/atuin-v${ATUIN_VERSION}-x86_64-unknown-linux-gnu/completions/atuin.fish" /usr/share/fish/vendor_completions.d/
+cp "${TMP_DIR}/atuin-v${ATUIN_VERSION}-x86_64-unknown-linux-gnu/completions/_atuin" /usr/share/zsh/site-functions/
+
+rm -rf "$TMP_DIR"
