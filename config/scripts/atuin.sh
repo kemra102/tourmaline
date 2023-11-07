7491df3191dc6b4371016f67301d7db51aa772cf
Danny Roberts <danny@thefallenphoenix.net>
2023-11-07 17:28:26 +0000

    fix code for pulling down atuin

--- config/scripts/atuin.sh
+++ config/scripts/atuin.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 ATUIN_VERSION='17.0.1'
+ATUIN_FILE_NAME="atuin-v${ATUIN_VERSION}-x86_64-unknown-linux-gnu" # without file extension
 TMP_DIR=$(mktemp -d)
 
-curl -s "https://github.com/atuinsh/atuin/releases/download/v${ATUIN_VERSION}/atuin-v${ATUIN_VERSION}-x86_64-unknown-linux-gnu.tar.gz" | tar -xzf - -C "$TMP_DIR"
+curl -sL "https://github.com/atuinsh/atuin/releases/download/v${ATUIN_VERSION}/${ATUIN_FILE_NAME}.tar.gz" --output - | tar -xzf - -C "$TMP_DIR"
 
-cp "${TMP_DIR}/atuin-v${ATUIN_VERSION}-x86_64-unknown-linux-gnu/atuin" /usr/bin/
-cp "${TMP_DIR}/atuin-v${ATUIN_VERSION}-x86_64-unknown-linux-gnu/completions/atuin.bash" /usr/share/bash-completion/completions/atuin
-cp "${TMP_DIR}/atuin-v${ATUIN_VERSION}-x86_64-unknown-linux-gnu/completions/atuin.fish" /usr/share/fish/vendor_completions.d/
-cp "${TMP_DIR}/atuin-v${ATUIN_VERSION}-x86_64-unknown-linux-gnu/completions/_atuin" /usr/share/zsh/site-functions/
+cp "${TMP_DIR}/${ATUIN_FILE_NAME}/atuin" /usr/bin/
+cp "${TMP_DIR}/${ATUIN_FILE_NAME}/completions/atuin.bash" /usr/share/bash-completion/completions/atuin
+cp "${TMP_DIR}/${ATUIN_FILE_NAME}/completions/atuin.fish" /usr/share/fish/vendor_completions.d/
+cp "${TMP_DIR}/${ATUIN_FILE_NAME}/completions/_atuin" /usr/share/zsh/site-functions/
 
 rm -rf "$TMP_DIR"
