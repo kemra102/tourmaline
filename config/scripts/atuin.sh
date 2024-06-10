cb479cd6d170582f1d807fb820e22748f24d8934
Danny Roberts <danny@thefallenphoenix.net>
2024-06-10 20:25:59 +0100

    shell completions from atuin are no longer in the release tarball

--- config/scripts/atuin.sh
+++ config/scripts/atuin.sh
@@ -9,8 +9,9 @@ TMP_DIR=$(mktemp -d)
 curl -sL "https://github.com/atuinsh/atuin/releases/download/v${ATUIN_VERSION}/${ATUIN_FILE_NAME}.tar.gz" --output - | tar -xzf - -C "$TMP_DIR"
 
 cp "${TMP_DIR}/${ATUIN_FILE_NAME}/atuin" /usr/bin/
-cp "${TMP_DIR}/${ATUIN_FILE_NAME}/completions/atuin.bash" /usr/share/bash-completion/completions/atuin
-cp "${TMP_DIR}/${ATUIN_FILE_NAME}/completions/atuin.fish" /usr/share/fish/vendor_completions.d/
-cp "${TMP_DIR}/${ATUIN_FILE_NAME}/completions/_atuin" /usr/share/zsh/site-functions/
+
+# Shell completions no longer included in release tarball
+# So generate them now
+/usr/bin/atuin gen-completions --shell zsh --out-dir /usr/share/zsh/site-functions/
 
 rm -rf "$TMP_DIR"
