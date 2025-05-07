528c5377d25d7ab88d6d784ab73e87200c8775a2
Danny Roberts <danny@thefallenphoenix.net>
2025-05-07 18:27:09 +0100

    bump atuin to 18.6.0

--- build_files/build.sh
+++ build_files/build.sh
@@ -49,7 +49,7 @@ dnf5 -y install k3b flac solaar vim-enhanced zsh
 
 
 # Install Atuin
-ATUIN_VERSION='18.5.0'
+ATUIN_VERSION='18.6.0'
 ATUIN_FILE_NAME="atuin-x86_64-unknown-linux-gnu" # without file extension
 TMP_DIR=$(mktemp -d)
 
